3686d21be16482cd6c01a20778b1c1a8a45937b5
tiagofeits <72151099+tiagofeits@users.noreply.github.com>
2020-10-01 10:41:10 -0300

    Tiago's Developer Test using AngularJS and .net MVC

---
+++ TiagosDeveloperTest/Tiago/Models/EF/TiagoDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Tiago.Models.EF
+{
+    public class TiagoDbContext : DbContext
+    {
+        public TiagoDbContext() : base("name = TiagoDbContext")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ TiagosDeveloperTest/Tiago/Models/EF/User.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Tiago.Models.EF
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(32)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [EmailAddress]
+        public string Email { get; set; }
+		
+		[Required]
+        public int Age { get; set; }
+
+        [StringLength(100, MinimumLength = 2)]
+        public string Address { get; set; }
+    }
+}
